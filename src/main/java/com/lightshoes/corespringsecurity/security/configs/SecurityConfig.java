8a115059ef1ef6f68cf13105989e5dc7a3eb6c94
kwang <shinkwang@yonsei.ac.kr>
2023-07-28 09:48:11 +0900

    feat: Custom form login page and logout

--- src/main/java/com/lightshoes/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/com/lightshoes/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.lightshoes.corespringsecurity.security.configs;
 
+import com.lightshoes.corespringsecurity.security.provider.CustomAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -51,6 +55,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         .and()
                 .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login_proc")
+                .permitAll()
         ;
     }
 }
