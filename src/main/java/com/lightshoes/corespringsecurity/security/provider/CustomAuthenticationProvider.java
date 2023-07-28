2808076a3d4f38e7967345d1af191fe2c23e3eb9
kwang <shinkwang@yonsei.ac.kr>
2023-07-28 11:24:26 +0900

    feat: Custom AuthenticationDetails

--- src/main/java/com/lightshoes/corespringsecurity/security/provider/CustomAuthenticationProvider.java
+++ src/main/java/com/lightshoes/corespringsecurity/security/provider/CustomAuthenticationProvider.java
@@ -1,9 +1,12 @@
 package com.lightshoes.corespringsecurity.security.provider;
 
+import com.lightshoes.corespringsecurity.security.common.FormAuthenticationDetailsSource;
+import com.lightshoes.corespringsecurity.security.common.FormWebAuthenticationDetails;
 import com.lightshoes.corespringsecurity.security.service.AccountContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -28,6 +31,14 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         if (!passwordEncoder.matches(password, accountContext.getAccount().getPassword())) {
             throw new BadCredentialsException("BadCredentialsException");
         }
+
+        FormWebAuthenticationDetails details = (FormWebAuthenticationDetails) authentication.getDetails();
+
+        String secretKey = details.getSecretKey();
+        if (secretKey == null || !"secret".equals(secretKey)) {
+            throw new InsufficientAuthenticationException("InsufficientAuthenticationException");
+        }
+
 
         UsernamePasswordAuthenticationToken authenticationToken
                 = UsernamePasswordAuthenticationToken.authenticated(accountContext.getAccount(), null, accountContext.getAuthorities());
