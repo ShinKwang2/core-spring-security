0a27909b05ee37682c76ded5893a7f783c2ea8bf
kwang <shinkwang@yonsei.ac.kr>
2023-08-01 11:17:38 +0900

    refactor: Separate SecurityConfig for ajax and form.

---
+++ src/main/java/com/lightshoes/corespringsecurity/security/configs/AjaxSecurityConfig.java
@@ -0,0 +1,68 @@
+package com.lightshoes.corespringsecurity.security.configs;
+
+import com.lightshoes.corespringsecurity.security.common.AjaxLoginAuthenticationEntryPoint;
+import com.lightshoes.corespringsecurity.security.filter.AjaxLoginProcessingFilter;
+import com.lightshoes.corespringsecurity.security.handler.AjaxAccessDeniedHandler;
+import com.lightshoes.corespringsecurity.security.handler.AjaxAuthenticationFailureHandler;
+import com.lightshoes.corespringsecurity.security.handler.AjaxAuthenticationSuccessHandler;
+import com.lightshoes.corespringsecurity.security.provider.AjaxAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+@Order(0)
+public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    private final PasswordEncoder passwordEncoder;
+
+
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        AuthenticationManager manager = http.getSharedObject(AuthenticationManager.class);
+        http
+                .antMatcher("/api/**")
+                .authorizeRequests()
+                .anyRequest().authenticated()
+        .and()
+                .addFilterBefore(ajaxLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable()
+
+
+        ;
+    }
+
+
+
+
+
+    @Bean
+    public AjaxLoginProcessingFilter ajaxLoginProcessingFilter() throws Exception {
+        AjaxLoginProcessingFilter ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter();
+        ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManagerBean());
+
+        return ajaxLoginProcessingFilter;
+    }
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
